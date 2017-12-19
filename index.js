dd3ea08bf44f53ee994db458d9730c5bb43bfc21
Timendainum <Timendainum@users.noreply.github.com>
2017-12-19 09:10:29 -0600

    adding more character tracking

--- index.js
+++ index.js
@@ -7,7 +7,10 @@
 const _ = require("lodash");
 const async = require("async");
 const Winston = require("winston");
-const log = new Winston.Logger({
+
+const config = require("./config");
+
+global.log = new Winston.Logger({
 	exitOnError: true,
 	debugStdout: true,
 	transports: [
@@ -21,19 +24,17 @@ const log = new Winston.Logger({
 	]
 });
 
-const config = require("./config");
-
-const DB = require("./lib/DB");
-DB.init(config, log);
+global.DB = require("./lib/DB");
+DB.init(config);
 
 global.Area = require("./lib/Area");
-Area.init(config, log, DB);
+Area.init(config);
 
 global.Player = require("./lib/Player");
-Player.init(config, log, DB);
+Player.init(config);
 
 global.Character = require("./lib/Character");
-Character.init(config, log, DB);
+Character.init(config);
 
 /* *********************************************************************************************************************
  * vars
@@ -79,7 +80,7 @@ function startHeartbeat() {
 }
 
 /**
- * heartBeat is to call slow update functions, mainly intended to retrieve commands from AI
+ * heartBeat is to call slow update functions
  * @param callback
  */
 function heartbeat(callback) {
@@ -90,6 +91,42 @@ function heartbeat(callback) {
 	], (err) => {
 		if (err) {
 			log.error("heartBeat() error", {err: err});
+		}
+		callback(err);
+	});
+}
+
+/**
+ * starts heartbeat running
+ * @returns {Promise}
+ */
+function startTick5Min() {
+	async.forever(
+		function (next) {
+			log.debug("startTick5Min() - forever iteration");
+			tick5Min(() => {
+				setTimeout(next, 300000);
+			});
+		},
+		function (err) {
+			if (err) {
+				log.error("startTick5Min error", {err: err});
+			}
+		}
+	);
+}
+
+/**
+ * tick5Min is called every 5 minutes
+ * @param callback
+ */
+function tick5Min(callback) {
+	log.warn("tick5Min");
+	async.series([
+		Area.deactivateStaleAreas
+	], (err) => {
+		if (err) {
+			log.error("tick5Min() error", {err: err});
 		}
 		callback(err);
 	});
